fda9130a78cea9ee98e3831b491298b7f2068d8f
D0rianGrey <utyfdfrthby08@gmail.com>
2025-02-25 19:05:48 +0100

    Add Allure steps to HomePage and LoginPage; update Jenkinsfile for Telegram notifications

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,15 +1,19 @@
 package pages;
 
+import io.qameta.allure.Step;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class HomePage {
 
+    @Step("Open Expedia homepage")
     public HomePage openPage() {
         open("/");
         return this;
     }
 
+    @Step("Check if Expedia homepage is loaded")
     public boolean isLoaded() {
         return $("body").exists();
     }
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,14 +1,18 @@
 package pages;
 
+import io.qameta.allure.Step;
+
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class LoginPage {
 
+    @Step("Open login page")
     public void openLoginPage() {
         open("/login");
     }
 
+    @Step("Login with email {email}")
     public void login(String email) {
         $("#loginFormEmailInput").shouldBe(visible).setValue(email);
         $("#loginFormSubmitButton").shouldBe(visible).click();
